fc6b67206f1bced2aeb1e6424160660e25b714e4
ValeryGusso <valery.gusso@yandex.ru>
2022-11-25 20:40:56 +0300

    fix mail

--- service/mail.js
+++ service/mail.js
@@ -22,11 +22,22 @@ class MailService {
 					from: process.env.MAIL_LOGIN,
 					to,
 					subject:
-						'Активация аккаунта на Niggflex. Лучшем в мире сервисе о кино. Niggflex - всё, что ты любишь, но немного темнее!',
+						'Активация аккаунта на Niggflex.',
 					text: '',
-					html: `<div>
-                <h1>${key}</h1>
-               </div>`,
+					html: `<div
+					style="display: flex; flex-direction: column; align-items: center; justify-content: center; background: #212325; text-align: center;"
+				>
+					<img
+						style="width: 45%"
+						src="https://cdn.discordapp.com/attachments/410547970505703436/1045749764555292752/logo.png"
+						alt="image"
+					/>
+					<h1 style="color: #ededed; padding: 15px 30px; border: 2px solid #ededed; border-radius: 15px">${key}</h1>
+					<h2 style="color: #ededed; padding: 15px 30px">
+						Это твой код для активации аккаунта на <span style="color: #E50914; font-style: italic">Niggflex</span> - лучшем в мире сайте о кино! <br />
+						<span style="color: #E50914; font-style: italic">Niggflex</span> - всё, что ты любишь, но немного темнее!
+					</h2>
+				</div>`,
 				},
 				err => {
 					if (err) {
